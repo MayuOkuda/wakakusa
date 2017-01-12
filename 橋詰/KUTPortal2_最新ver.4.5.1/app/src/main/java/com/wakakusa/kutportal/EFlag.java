6ce2ad7582c8d6899da3a6204329deb5cd29cee9
hassin4649 <hassin123@iCloud.com>
2017-01-12 15:57:45 +0900

    add logout

---
+++ 橋詰/KUTPortal2_最新ver.4.5.1/app/src/main/java/com/wakakusa/kutportal/EFlag.java
@@ -0,0 +1,13 @@
+package com.wakakusa.kutportal;
+
+/**
+ * Created by hashidzumeshinya on 2017/01/05.
+ */
+
+public class EFlag {
+    private boolean flag;
+
+    public void setFlagState(boolean f){ flag = f;}
+
+    public boolean getFlagState(){return flag;}
+}
