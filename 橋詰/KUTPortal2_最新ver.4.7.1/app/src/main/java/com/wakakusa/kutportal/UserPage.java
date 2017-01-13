10b136c0107a0b2551f4d84e048076e1e82e4f86
hassin4649 <hassin123@iCloud.com>
2017-01-14 05:26:31 +0900

    syusei

---
+++ 橋詰/KUTPortal2_最新ver.4.7.1/app/src/main/java/com/wakakusa/kutportal/UserPage.java
@@ -0,0 +1,89 @@
+package com.wakakusa.kutportal;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.*;
+
+public class UserPage extends BasePage {
+
+    final String[] student_property = {"id","name", "birth","adm","ug","dpm","mjr","sub1","sub2","teacher","address","mailaddress"};
+    final int[] id_box = {R.id.userinfo1, R.id.userinfo2, R.id.userinfo3,R.id.userinfo4,R.id.userinfo5
+                                ,R.id.userinfo6,R.id.userinfo7,R.id.userinfo8,R.id.userinfo9,R.id.userinfo10,R.id.userinfo11,R.id.userinfo12};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        userinfo();
+
+    }
+    public void onCheckboxClicked(View view) {
+        final boolean checked = ((CheckBox) view).isChecked();
+        switch(view.getId()) {
+            case R.id.checkBox2:
+                if (checked) {
+                    // チェックボックス1がチェックされる
+                } else {
+                    // チェックボックス1のチェックが外される
+                }
+                break;
+            case R.id.checkBox3:
+                if (checked) {
+                    // チェックボックス2がチェックされる
+                } else {
+                    // チェックボックス2のチェックが外される
+                }
+                break;
+        }
+    }
+
+
+    //データベースからユーザ情報を記述するためのメソッド
+    void userinfo(){
+
+        //学籍番号と名前の表示
+        DatabaseReader r_db = new DatabaseReader(this, "student");
+        String s[] = new String[12];
+        for(int i=0; i< 12; i++)s[i] = "null";
+        String[] db_s = r_db.readDB(student_property, 0).split("\n",0);
+        int s_num = 0;
+        for(String str : db_s) {
+            s[s_num] = str;
+            s_num++;
+        }
+        int num = 0;
+        for(int i : id_box){
+            System.out.println(i);
+            System.out.println(s[num]);
+            TextView tex1 = (TextView) findViewById(i);
+            if(s[num].equals("null"))tex1.setText("　　");
+            else tex1.setText(" " + s[num]);
+            num++;
+        }
+
+    }
+
+}
---
+++ 橋詰/KUTPortal2_最新ver.4.7.1/app/src/main/java/com/wakakusa/kutportal/tab3.java
@@ -0,0 +1,404 @@
+package com.wakakusa.kutportal;
+
+import android.app.AlertDialog;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+public class tab3 extends Fragment {
+
+
+    View rootView;
+
+    //popに使う
+    Today pop[][] = new Today[6][5];
+    private View.OnClickListener[] onClick_textview = new View.OnClickListener[30];
+
+    int[][] ID = {{R.id.time3_a1, R.id.time3_a2, R.id.time3_a3, R.id.time3_a4, R.id.time3_a5},
+            {R.id.time3_b1, R.id.time3_b2, R.id.time3_b3, R.id.time3_b4, R.id.time3_b5},
+            {R.id.time3_c1, R.id.time3_c2, R.id.time3_c3, R.id.time3_c4, R.id.time3_c5},
+            {R.id.time3_d1, R.id.time3_d2, R.id.time3_d3, R.id.time3_d4, R.id.time3_d5},
+            {R.id.time3_e1, R.id.time3_e2, R.id.time3_e3, R.id.time3_e4, R.id.time3_e5},
+            {R.id.time3_f1, R.id.time3_f2, R.id.time3_f3, R.id.time3_f4, R.id.time3_f5}};
+
+    public tab3() {
+    }
+
+    public static tab3 newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt("page", page);
+        tab3 fragment = new tab3();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        int page = getArguments().getInt("page", 0);
+        rootView = inflater.inflate(R.layout.activity_tab3, container, false);
+        CoursePage.couse_appearance("3Q");
+        SetTextView();
+
+        return rootView;
+    }
+
+    //履修状況表示
+    void SetTextView() {
+        textviewReset();
+        Today t = CoursePage.course;
+        //回す
+        while (t.scode != null) {
+            for (String time : t.daytime) {
+                if (time.substring(0, 1).equals("月")) {
+                    writeClass(time, t, 0);
+
+                } else if (time.substring(0, 1).equals("火")) {
+                    writeClass(time, t, 1);
+
+                } else if (time.substring(0, 1).equals("水")) {
+                    writeClass(time, t, 2);
+
+                } else if (time.substring(0, 1).equals("木")) {
+                    writeClass(time, t, 3);
+
+                } else if (time.substring(0, 1).equals("金")) {
+                    writeClass(time, t, 4);
+
+                } else if (time.substring(0, 1).equals("土")) {
+                    writeClass(time, t, 5);
+                }
+            }
+            t = t.next;
+
+        }
+        //popのための処理
+        tabpop();
+
+    }
+
+    //内容書き込み
+    void writeClass(String time, Today t, int i) {
+        int classtime = Integer.parseInt(time.substring(1, 2));
+        TextView text = (TextView) rootView.findViewById(ID[i][classtime - 1]);
+        text.setText(t.subject);
+        pop[i][classtime - 1] = t;
+    }
+
+
+    //ダイアログ表示のメソッド
+    void ClassDialog(Today t) {
+        new AlertDialog.Builder(getActivity())
+                .setTitle(t.subject)
+                .setMessage("教室　　：" + t.room + "\n" + "担当教員：" + t.teacher + "\n" + "単位区分：" + t.sj + "\n" + "単位数　：" + t.sjclass)
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    //内容のリセット
+    void textviewReset() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                TextView text = (TextView) rootView.findViewById(ID[i][j]);
+                text.setText("          ");
+                pop[i][j] = null;
+            }
+        }
+    }
+
+
+    //以下pop表示のためのメソッド;
+    void tabpop() {
+        onClick_textview[0] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 0;
+                int b = 0;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[1] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 0;
+                int b = 1;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[2] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 0;
+                int b = 2;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[3] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 0;
+                int b = 3;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[4] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 0;
+                int b = 4;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+
+
+        onClick_textview[5] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 1;
+                int b = 0;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[6] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 1;
+                int b = 1;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[7] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 1;
+                int b = 2;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[8] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 1;
+                int b = 3;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[9] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 1;
+                int b = 4;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+
+        onClick_textview[10] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 2;
+                int b = 0;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[11] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 2;
+                int b = 1;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[12] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 2;
+                int b = 2;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[13] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 2;
+                int b = 3;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[14] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 2;
+                int b = 4;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+
+        onClick_textview[15] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 3;
+                int b = 0;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[16] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 3;
+                int b = 1;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[17] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 3;
+                int b = 2;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[18] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 3;
+                int b = 3;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[19] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 3;
+                int b = 4;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+
+        onClick_textview[20] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 4;
+                int b = 0;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[21] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 4;
+                int b = 1;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[22] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 4;
+                int b = 2;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[23] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 4;
+                int b = 3;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[24] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 4;
+                int b = 4;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+
+        onClick_textview[25] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 5;
+                int b = 0;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[26] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 5;
+                int b = 1;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[27] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 5;
+                int b = 2;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[28] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 5;
+                int b = 3;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+        onClick_textview[29] = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int a = 5;
+                int b = 4;
+                if (pop[a][b] != null)
+                    ClassDialog(pop[a][b]);
+            }
+        };
+
+        int p = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 5; j++) {
+                TextView text = (TextView) rootView.findViewById(ID[i][j]);
+                text.setOnClickListener(onClick_textview[p]);
+                ;
+                p++;
+            }
+        }
+
+    }
+
+
+}
